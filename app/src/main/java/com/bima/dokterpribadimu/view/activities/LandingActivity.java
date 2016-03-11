0ec1f4d3f30dda7f8384ddb3e77efb6b8ee43b49
apradanas <adit@apradanas.com>
2016-03-12 02:45:18 +0700

    Remove unused import

--- app/src/main/java/com/bima/dokterpribadimu/view/activities/LandingActivity.java
+++ app/src/main/java/com/bima/dokterpribadimu/view/activities/LandingActivity.java
@@ -2,7 +2,6 @@ package com.bima.dokterpribadimu.view.activities;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -41,7 +40,6 @@ public class LandingActivity extends BaseActivity {
     private ActivityLandingBinding binding;
 
     private LoginClient loginClient;
-    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
