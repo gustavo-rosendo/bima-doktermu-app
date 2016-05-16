ab3822c46fb56161c3edc368d7d0d2bc5c12abb8
apradanas <adit@apradanas.com>
2016-05-17 00:36:07 +0700

    Add PartnersSearchActivity

---
+++ app/src/main/java/com/bima/dokterpribadimu/view/activities/PartnersSearchActivity.java
@@ -0,0 +1,15 @@
+package com.bima.dokterpribadimu.view.activities;
+
+import android.os.Bundle;
+
+import com.bima.dokterpribadimu.R;
+import com.bima.dokterpribadimu.view.base.BaseActivity;
+
+public class PartnersSearchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partners_search);
+    }
+}
