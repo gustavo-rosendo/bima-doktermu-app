2a01088d8a9a8c4df47809bc3505f2e300541f2f
gustavo.rosendo <gustavo.rosendo@gmail.com>
2016-06-30 12:33:32 +0700

    Resolve issue #75

--- app/src/main/java/com/bima/dokterpribadimu/viewmodel/CategoryItemViewModel.java
+++ app/src/main/java/com/bima/dokterpribadimu/viewmodel/CategoryItemViewModel.java
@@ -2,6 +2,7 @@ package com.bima.dokterpribadimu.viewmodel;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
 import android.view.View;
 
 import com.bima.dokterpribadimu.model.Category;
@@ -17,6 +18,16 @@ public class CategoryItemViewModel extends BaseObservable {
     public CategoryItemViewModel(Category category, View.OnClickListener clickListener) {
         this.category = category;
         this.clickListener = clickListener;
+    }
+
+    @Bindable
+    public boolean isSelected() {
+        return category.isSelected();
+    }
+
+    @BindingAdapter("bind:selected")
+    public static void setViewSelected(View view, boolean selected) {
+        view.setSelected(selected);
     }
 
     @Bindable
