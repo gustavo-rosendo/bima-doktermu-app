b3c855f7de84151d47eeb1dfe338f5af1fbad288
Gustavo M. R. Santos <gustavo.rosendo@gmail.com>
2016-10-13 00:11:21 +0700

    Rowback rating implementation in the DoctorCall screen

--- app/src/main/java/com/bima/dokterpribadimu/view/fragments/DoctorCallFragment.java
+++ app/src/main/java/com/bima/dokterpribadimu/view/fragments/DoctorCallFragment.java
@@ -102,7 +102,11 @@ public class DoctorCallFragment extends BaseFragment {
 
         AnalyticsHelper.logViewScreenEvent(EventConstants.SCREEN_DOCTOR_CALL);
 
-
+        if(canShowRateYourCall()) {
+            if(rateYourCallDialog != null) {
+                rateYourCallDialog.showDialog();
+            }
+        }
     }
 
     @Override
@@ -174,6 +178,18 @@ public class DoctorCallFragment extends BaseFragment {
             }
         });
 
+        if(rateYourCallDialog == null) {
+            rateYourCallDialog = new RateYourCallDialog(getActivity());
+        }
+        rateYourCallDialog.setListener(new RateYourCallDialog.OnRateYourCallDialogClickListener() {
+            @Override
+            public void onClick(RateYourCallDialog dialog, float rating) {
+                dismissRateYourCallDialog();
+
+                Integer ratingInt = Integer.valueOf(Math.round(rating));
+                rateCall(String.valueOf(lastCallId), ratingInt, UserProfileUtils.getUserProfile(getActivity()).getAccessToken());
+            }
+        });
     }
 
     private void checkServerTime() {
@@ -241,6 +257,99 @@ public class DoctorCallFragment extends BaseFragment {
         }
 
         return isValidTime;
+    }
+
+
+    /**
+     * Check if the minimum time between calls has already passed and should show Rate Your Call dialog
+     * @return boolean true if rate your call dialog can already be shown, boolean false if otherwise
+     */
+    private boolean canShowRateYourCall() {
+        double lastBookedCallTimeMillis = StorageUtils.getDouble(
+                DokterPribadimuApplication.getInstance().getApplicationContext(),
+                Constants.KEY_BOOK_CALL_TIME_MILLIS,
+                -1);
+        //if it's not found, it shouldn't show the rate call dialog
+        if(lastBookedCallTimeMillis == -1) {
+            return false;
+        }
+
+        String callId = StorageUtils.getString(
+                DokterPribadimuApplication.getInstance().getApplicationContext(),
+                Constants.KEY_BOOK_CALL_ID_LAST_CALL,
+                "0");
+
+        this.lastCallId = 0;
+        try {
+            this.lastCallId = Integer.parseInt(callId);
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Couldn't get the last call id: " + callId
+                    + " - error message: "
+                    + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+
+        boolean canShowRateYourCall = false;
+        if(this.lastCallId != 0 && TimeUtils.hasOneHourPassed(lastBookedCallTimeMillis)) {
+            canShowRateYourCall = true;
+        }
+
+        return canShowRateYourCall;
+    }
+
+    private void dismissRateYourCallDialog() {
+        rateYourCallDialog.dismiss();
+        rateYourCallDialog.clearReference();
+        rateYourCallDialog = null;
+    }
+
+
+    /**
+     * Rate the call received in the last hour
+     * @param callId Id of the last call
+     * @param rating Rating given by the user
+     */
+    private void rateCall(final String callId, final Integer rating, final String accessToken) {
+        rateYourCallApi.rateCall(callId, rating, accessToken)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .compose(this.<BaseResponse>bindToLifecycle())
+                .subscribe(new Subscriber<BaseResponse>() {
+
+                    @Override
+                    public void onStart() {
+                        showProgressDialog();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        dismissProgressDialog();
+
+                        handleError(TAG, e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(BaseResponse rateCallResponse) {
+                        dismissProgressDialog();
+
+                        if (rateCallResponse.getStatus() == Constants.Status.SUCCESS) {
+                            showSuccessDialog(
+                                    R.drawable.ic_smiley,
+                                    getString(R.string.dialog_rate_your_call_success),
+                                    getString(R.string.dialog_rate_your_call_success_message),
+                                    getString(R.string.ok),
+                                    null);
+                            AnalyticsHelper.logViewDialogRatingEvent(EventConstants.DIALOG_DOCTOR_CALL_RATING_SUCCESS, rating);
+                        } else {
+                            handleError(TAG, rateCallResponse.getMessage());
+                        }
+                    }
+                });
     }
 
 }
